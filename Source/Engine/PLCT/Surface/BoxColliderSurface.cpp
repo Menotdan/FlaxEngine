db475eca0cf14a1d4a4fc65cdad2e75f6a7cc500
Menotdan <32620310+Menotdan@users.noreply.github.com>
2023-12-25 11:07:24 -0500

    Properly calculate the point position on a box collider.

--- Source/Engine/PLCT/Surface/BoxColliderSurface.cpp
+++ Source/Engine/PLCT/Surface/BoxColliderSurface.cpp
@@ -1,13 +1,50 @@
 #include "BoxColliderSurface.h"
 
-PLCTPoint* BoxColliderSurface::SampleXZ(Vector2 coordinates)
+PLCTPoint* BoxColliderSurface::SampleXZ(Vector2 xz)
 {
+    if (!_actor)
+    {
+        return nullptr;
+    }
+
     Vector3 pointFound;
 
-    BoundingSphere actorSphere = _actor->GetSphere();
-    Vector3 pointToSearchFrom = {coordinates.X, actorSphere.Center.Y + actorSphere.Radius, coordinates.Y};
-    _actor->ClosestPoint(pointToSearchFrom, pointFound);
+    OrientedBoundingBox actorBox = _actor->GetOrientedBox();
+    Vector3 corners[8];
+    actorBox.GetCorners(corners);
 
+    Face faces[6];
+    faces[0] = { corners[0], corners[1], corners[2], corners[3]}; // top
+    faces[1] = { corners[4], corners[5], corners[6], corners[7] }; // bottom
+    faces[2] = { corners[4], corners[0], corners[3], corners[7] }; // front
+    faces[3] = { corners[6], corners[2], corners[1], corners[5] }; // back
+    faces[4] = { corners[5], corners[1], corners[0], corners[4] }; // left
+    faces[5] = { corners[7], corners[3], corners[2], corners[6] }; // right
+
+    float highestY = 0;
+    bool foundAny = false;
+    for (int face = 0; face < 6; face++)
+    {
+        if (!faces[face].IsWithinBounds(xz))
+        {
+            continue;
+        }
+
+        float y = faces[face].GetYAt(xz);
+        if (y > highestY || !foundAny)
+        {
+            highestY = y;
+            pointFound = Vector3(xz.X, y, xz.Y);
+        }
+
+        foundAny = true;
+    }
+
+    if (!foundAny)
+    {
+        return nullptr;
+    }
+    
     Transform pointTransform = Transform::Identity;
     pointTransform.Translation = pointFound;
     pointTransform.Orientation = _actor->GetOrientation();
@@ -20,7 +57,14 @@ PLCTPoint* BoxColliderSurface::SampleXZ(Vector2 coordinates)
 
 bool BoxColliderSurface::SampleXZ(Vector2 coordinates, PLCTPointsContainer* targetContainer)
 {
-    return false;
+    PLCTPoint* point = SampleXZ(coordinates);
+    if (point == nullptr)
+    {
+        return false;
+    }
+    
+    targetContainer->GetPoints().Add(*point);
+    return true;
 }
 
 bool BoxColliderSurface::CheckActorMatchesAndSet(Actor* actor)
--- Source/Engine/PLCT/Surface/BoxColliderSurface.h
+++ Source/Engine/PLCT/Surface/BoxColliderSurface.h
@@ -8,6 +8,38 @@
 #include "Engine/Core/Math/Vector3.h"
 #include "../PLCTSurface.h"
 
+struct Face
+{
+    Vector3 edge1;
+    Vector3 edge2;
+    Vector3 edge3;
+    Vector3 edge4;
+
+    bool IsWithinBounds(Vector2 xz)
+    {
+        Vector2 AB = Vector2(edge2.X, edge2.Z) - Vector2(edge1.X, edge1.Z);
+        Vector2 AM = xz - Vector2(edge1.X, edge1.Z);
+        Vector2 BC = Vector2(edge3.X, edge3.Z) - Vector2(edge2.X, edge2.Z);
+        Vector2 BM = xz - Vector2(edge2.X, edge2.Z);
+
+        float dot1 = Vector2::Dot(AB, AM);
+        float dot2 = Vector2::Dot(AB, AB);
+        float dot3 = Vector2::Dot(BC, BM);
+        float dot4 = Vector2::Dot(BC, BC);
+        return 0 <= dot1 && dot1 <= dot2 &&
+            0 <= dot3 && dot3 <= dot4;
+    }
+
+    float GetYAt(Vector2 xz)
+    {
+        Vector3 v1 = edge1 - edge2;
+        Vector3 v2 = edge1 - edge3;
+        Vector3 n = Vector3::Cross(v1, v2);
+
+        return (1 / Math::Max(n.Y, 0.0001f)) * ((n.X * edge1.X) + (n.Y * edge1.Y) + (n.Z * edge1.Z) - (n.X * xz.X) - (n.Z * xz.Y));
+    }
+};
+
 /// <summary>
 /// Box collider PLCT surface.
 /// </summary>
@@ -21,5 +53,5 @@ public:
     bool SampleXZ(Vector2 coordinates, PLCTPointsContainer* targetContainer) override;
     bool CheckActorMatchesAndSet(Actor* actor) override;
 private:
-    BoxCollider* _actor;
+    BoxCollider* _actor = nullptr;
 };
